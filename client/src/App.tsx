eeba2fcf3556a1f85f5d5105aa26bd5d07daab51
Vladimir Litvintsev <vl.litvintsev@yandex.ru>
2022-10-01 19:49:17 +0700

    html code done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import { Auth } from "./views/Auth";
 import { Home } from "./views/Home";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<Auth />} />
     </Routes>
   );
 };
